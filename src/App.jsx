a70bf2334f5492ae77a4b0c6afb18f979fba0344
Yhordi Choque <yordychoque124@gmail.com>
2023-11-09 19:21:37 -0400

    he creado otro fetch que solicita la url img de gato.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import { useEffect, useState } from "react"
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGEN_URL = 'https://cataas.com/cat/says/'
+
 export const App = () => {
  
    const [fact,setFact] = useState('lorem ipsum cat fact whatever')
+   const [urlImg, setUrlImg] = useState()
 
    useEffect(()=>{
     fetch(CAT_ENDPOINT_RANDOM_FACT)
     .then(res => res.json())
-    .then(data => setFact(data.fact))
-   },[])
+    .then(data => {
+        const {fact } = data
+        setFact(fact)
+        const threeFirstWords = fact.split('',3).join('')
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+    .then(res => res.json())
+    .then(responde => {
+        const {mimetype}= responde
+     
+        setUrlImg(`${CAT_PREFIX_IMAGEN_URL}${threeFirstWords}?${mimetype}`)
+    })
+    })
+
+    
+   
+}
+   ,[])
     return (
-        <>
+        <main>
         <h1>Prueba tegnica</h1>
-        <p>{fact}</p>
-        </>
+        {fact && <p>{fact}</p>}
+        {urlImg && <img src={urlImg} alt={`ìmagen de gatito diciendo ${fact}`}/>}
+        </main>
         
     )
 }
