7f2746d08da5b910b31353a524a5e32050dcbd21
Yhordi Choque <yordychoque124@gmail.com>
2023-11-09 20:14:32 -0400

    separe mi useEffect en dos, di estilos a la pagina centreandola

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-
+import './App.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_PREFIX_IMAGEN_URL = 'https://cataas.com/cat/says/'
 
@@ -8,32 +8,36 @@ export const App = () => {
    const [fact,setFact] = useState('lorem ipsum cat fact whatever')
    const [urlImg, setUrlImg] = useState()
 
+   // Recupera la cita al iniciar la pagina
    useEffect(()=>{
     fetch(CAT_ENDPOINT_RANDOM_FACT)
     .then(res => res.json())
     .then(data => {
         const {fact } = data
         setFact(fact)
-        const threeFirstWords = fact.split('',3).join('')
+    })
+    }
+   ,[])
+// recupera la imagen cada vez que tenemos una cita nueva
+   useEffect(()=>{
+    if(!fact) return
+    const threeFirstWords = fact.split('',3).join('')
 
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
     .then(res => res.json())
     .then(responde => {
-        const {mimetype}= responde
-     
-        setUrlImg(`${CAT_PREFIX_IMAGEN_URL}${threeFirstWords}?${mimetype}`)
-    })
-    })
-
-    
-   
-}
-   ,[])
+    const {mimetype}= responde
+ 
+    setUrlImg(`${CAT_PREFIX_IMAGEN_URL}${threeFirstWords}?${mimetype}`)
+})
+   },[fact])
     return (
         <main>
         <h1>Prueba tegnica</h1>
-        {fact && <p>{fact}</p>}
+       <section>
+       {fact && <p>{fact}</p>}
         {urlImg && <img src={urlImg} alt={`ìmagen de gatito diciendo ${fact}`}/>}
+       </section>
         </main>
         
     )
